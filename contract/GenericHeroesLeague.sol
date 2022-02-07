6d95948d2b0e6d1bc780bdeb2e65689d64ce3d6f
luizraphaeldp <luizraphaeldp@gmail.com>
2022-02-07 17:03:27 -0300

    Update and rename SimpleNftLowerGas.sol to GenericHeroesLeague.sol

--- contract/GenericHeroesLeague.sol
+++ contract/GenericHeroesLeague.sol
@@ -1,19 +1,5 @@
 // SPDX-License-Identifier: MIT
-
-// Amended by HashLips
-/**
-    !Disclaimer!
-
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    The developer will not be responsible or liable for all loss or 
-    damage whatsoever caused by you participating in any way in the 
-    experimental code, whether putting money into the contract or 
-    using the code for your own project.
-*/
+// SPDX-License-Identifier: MIT
 
 pragma solidity >=0.7.0 <0.9.0;
 
@@ -21,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SimpleNftLowerGas is ERC721, Ownable {
+contract GenericHeroesLeague is ERC721, Ownable {
   using Strings for uint256;
   using Counters for Counters.Counter;
 
@@ -31,14 +17,14 @@ contract SimpleNftLowerGas is ERC721, Ownable {
   string public uriSuffix = ".json";
   string public hiddenMetadataUri;
   
-  uint256 public cost = 0.01 ether;
-  uint256 public maxSupply = 10000;
+  uint256 public cost = 0.06 ether;
+  uint256 public maxSupply = 6990;
   uint256 public maxMintAmountPerTx = 5;
 
   bool public paused = true;
   bool public revealed = false;
 
-  constructor() ERC721("NAME", "SYMBOL") {
+  constructor() ERC721("Generic Heroes League", "GHL") {
     setHiddenMetadataUri("ipfs://__CID__/hidden.json");
   }
 
@@ -139,19 +125,8 @@ contract SimpleNftLowerGas is ERC721, Ownable {
   }
 
   function withdraw() public onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-
-    // This will transfer the remaining contract balance to the owner.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
-    // =============================================================================
   }
 
   function _mintLoop(address _receiver, uint256 _mintAmount) internal {
